7353d511cb417099f2dbc8ce0f29974e225b47c3
Jason Yeung <jsy9@sfu.ca>
2017-03-04 02:28:32 -0800

    add FAB

--- app/src/main/java/com/as3/parmjohal/carbontracker/MainActivity.java
+++ app/src/main/java/com/as3/parmjohal/carbontracker/MainActivity.java
@@ -1,11 +1,19 @@
 package com.as3.parmjohal.carbontracker;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private Boolean isFabOpen = false;
+    private FloatingActionButton fab, fab_transport;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,47 @@ public class MainActivity extends AppCompatActivity {
             VehicleData v = new VehicleData(this);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        // add icon to dashboard action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.mipmap.app_icon_white);
+
+        // set FAB
+        setFAB();
+    }
+
+    // set Floating Action Button
+    public void setFAB() {
+        fab = (FloatingActionButton) findViewById(R.id.fab_main);
+        fab_transport = (FloatingActionButton) findViewById(R.id.fab_transport);
+
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animateFAB();
+            }
+        });
+    }
+
+    public void animateFAB(){
+        if(isFabOpen){
+            fab.startAnimation(rotate_backward);
+            fab_transport.startAnimation(fab_close);
+            fab_transport.setClickable(false);
+            isFabOpen = false;
+
+        } else {
+            fab.startAnimation(rotate_forward);
+            fab_transport.startAnimation(fab_open);
+            fab_transport.setClickable(true);
+            isFabOpen = true;
         }
     }
 }
