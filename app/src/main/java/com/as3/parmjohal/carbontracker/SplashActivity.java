21c86e517cfa17996cb0beaa8f65bb7c23470fbd
Jason Yeung <jsy9@sfu.ca>
2017-03-03 20:17:19 -0800

    Set up Splash and Dashboard Activities

---
+++ app/src/main/java/com/as3/parmjohal/carbontracker/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.as3.parmjohal.carbontracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        hideNavigation();
+    }
+
+    public void hideNavigation() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+}
