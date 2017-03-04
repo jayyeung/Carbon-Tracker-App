4da38768b7ec46bbcb1ee2331158a2b29aa46b3b
Jason Yeung <jsy9@sfu.ca>
2017-03-03 22:40:13 -0800

    add splash animations

--- app/src/main/java/com/as3/parmjohal/carbontracker/SplashActivity.java
+++ app/src/main/java/com/as3/parmjohal/carbontracker/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.as3.parmjohal.carbontracker;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,14 +15,17 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        playAnimations();
     }
 
     // set immersive mode
+    // this hides the status and navigation mode
     @Override
-    public void onWindowFocusChanged(boolean hasFocas) {
-        super.onWindowFocusChanged(hasFocas);
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
         View decorView = getWindow().getDecorView();
-        if(hasFocas) {
+        if(hasFocus) {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -25,5 +33,29 @@ public class SplashActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
+    }
+
+    // play animations
+    public void playAnimations() {
+        ImageView icon = (ImageView) findViewById(R.id.icon),
+                  title = (ImageView) findViewById(R.id.title);
+
+        Button new_journey = (Button) findViewById(R.id.new_journey_btn),
+               to_dash = (Button) findViewById(R.id.continue_dashboard);
+
+        // animations
+        Animation fade_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        fade_in.setDuration(1500);
+
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in);
+        slide_in.setStartOffset(1000);
+        slide_in.setDuration(1000);
+
+        // execute
+        icon.startAnimation(fade_in);
+        title.startAnimation(fade_in);
+
+        new_journey.startAnimation(slide_in);
+        to_dash.startAnimation(fade_in);
     }
 }
